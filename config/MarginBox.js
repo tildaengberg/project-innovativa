a9095cb170b524955256b91b99283823db1163ea
Tilda Engberg <tildaengberg@MacBook-Pro-som-tillhor-Tilda.local>
2022-12-06 16:40:59 +0100

    config av färger, font och marginaler

---
+++ config/MarginBox.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+// ! Config for correct margins
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 50,
+    paddingHorizontal: 16,
+  },
+})
