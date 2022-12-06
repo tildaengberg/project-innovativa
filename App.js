5864ca3fbf664f60926877201850df577e68fc00
Tilda Engberg <tildaengberg@MacBook-Pro-som-tillhor-Tilda.local>
2022-12-06 10:07:07 +0100

    testar att skapa branch

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button } from './components/Button'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hejsan hoppsan!</Text>
+      <Button />
       <StatusBar style='auto' />
     </View>
   )
