5bb235352c6caa779a1d5f93dc47891a443999f3
Emmy Lindgren <emmy.lindgren@hotmail.com>
2022-12-13 12:08:13 +0100

    post

--- components/TextPost.js
+++ components/TextPost.js
@@ -1,7 +1,10 @@
 import { View, StyleSheet, Text,TouchableWithoutFeedback} from 'react-native'
 import colors from '../config/colors'
 import StyledText from '../config/StyledText'
-{/* <Moodstory name= 'Agneta' emoji = '&#x1F600;' onPress={() => console.log('Click på emoji')}/> */}
+
+{/* <Moodstory name= 'Agneta' emoji = '&#x1F600;' onPress={() => console.log('Click på emoji')}/> 
+        <Like color = 'black' onPress ={()=> console.log("klick hjärta")}>2</Like>
+        <Comments color = 'black' onPress ={()=> console.log("klick kommentar")}>2</Comments>*/}
 
 const TextPost = () => {
   return (
@@ -9,6 +12,7 @@ const TextPost = () => {
         <StyledText textStyle='bodySmall' textAlign='left' textColor={colors.background.contrastColor}>
             {"Lorem ipsum dolor sit amet consectetur. Dictum quam non fringilla bibendum adipiscing suspendisse urna. Iaculis metus faucibus maecenas aliquam."}
         </StyledText>
+
     </View>
   )
 }
@@ -19,9 +23,9 @@ const styles = StyleSheet.create({
   textPost: {
     alignItems:'center',
     justifyContent:'center',
-    borderRadius: 12,
+    padding:15,
+    borderRadius: 15,
     width: '100%',
-    height: 146,
     backgroundColor: colors.background.main
   }
 })
