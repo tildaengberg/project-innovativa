22818e4229928fa4dbbb49d9bdfd9bb917d3e02a
Mats Bennervall <matsbennervall@gmail.com>
2022-12-13 11:02:56 +0100

    Made profile picture float left and made the summary component

---
+++ components/Summary.js
@@ -0,0 +1,30 @@
+import { View, StyleSheet, TouchableWithoutFeedback, Image } from 'react-native'
+import ProfilePic from './ProfilePic'
+{/*<Summary profile={require("./assets/profile.png")} emoji='&#128546;' picture={require("./assets/favicon.png")} image_size={120} />*/}
+
+const Summary = ({ onPress, disabled = false, profile, emoji, picture, image_size = 100 }) => {
+  return (
+    <TouchableWithoutFeedback disabled={disabled} onPress={onPress}>
+      <View style={[styles.holder]}>
+        <ProfilePic profile={profile} emoji={emoji} size={image_size} float='left' />
+        <Image source={picture} style={[styles.image,{width:image_size, height:image_size, borderRadius: image_size}]} />
+      </View>
+    </TouchableWithoutFeedback>
+  )
+}
+
+export default Summary
+
+const styles = StyleSheet.create({
+  holder: {
+    marginBottom: 8,
+    padding: 16,
+    borderRadius: 12,
+    width: '100%',
+    display: 'block',
+  },
+  image: {
+    position: 'relative',
+    float: 'right',
+  }
+})
