a4fde7cab945af949e7e2ebeffef69f45f9975f5
Tilda Engberg <tildaengberg@client212-106.wireless.umu.se>
2022-12-13 10:55:29 +0100

    navbar, icons pack

---
+++ screens/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+import MarginBox from '../config/MarginBox'
+import StyledText from '../config/StyledText'
+
+const Profile = () => {
+  return (
+    <View style={MarginBox.container}>
+      <StyledText>Profil</StyledText>
+    </View>
+  )
+}
+
+export default Profile
