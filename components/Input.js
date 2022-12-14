940484e8e8660a40f0ed9eb53d8d39c33e280ec2
alexwahlroos <alex.wahlroos@hotmail.com>
2022-12-14 14:23:07 +0100

    Added text input component

---
+++ components/Input.js
@@ -0,0 +1,28 @@
+import { StyleSheet, TextInput } from 'react-native'
+
+const Input = ({textInputValue, setTextInputValue}) => {
+
+    return (
+      <TextInput
+        multiline
+        style={styles.textField}
+        onChangeText={text => setTextInputValue(text)}
+        value={textInputValue}
+        placeholder="Skriv din text här..."
+      />
+    );
+}
+
+export default Input
+
+const styles = StyleSheet.create({
+    textField: {
+        maxHeight:100,
+        padding:10,
+        height: 65,
+        borderColor:'grey',
+        borderWidth: 1,
+        placeholderTextColor: 'gray',
+        borderRadius:'10',
+    },
+  })
