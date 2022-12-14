30cf35bd927009f528ae48e638098e71d0676810
alexwahlroos <alex.wahlroos@hotmail.com>
2022-12-14 14:26:28 +0100

    Added first page to Home, move to own page later

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,67 @@
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import MarginBox from '../config/MarginBox'
 import StyledText from '../config/StyledText'
+import Input from '../components/Input'
+import ProfilePic from '../components/ProfilePic'
+import MoodPicker from '../components/MoodPicker'
+import Button from '../components/Button'
+import { useState } from 'react'
+
 
 const Home = () => {
+
+  const [chosenEmoji, setChosenEmoji] = useState("");
+  const [textInputValue, setTextInputValue] = useState("");
+
+  const onPressNext = () => {
+    //Skicka detta data
+    console.log(chosenEmoji);
+    console.log(textInputValue);
+  }
+
+  const onPressNotNow = () => {
+    console.log("not now");
+  }
+
   return (
     <View style={MarginBox.container}>
-      <StyledText>Hem</StyledText>
+      <View style={styles.fullHeight}>
+        <StyledText >Hem</StyledText>
+        <StyledText textAlign={'center'} textStyle={'h3'} style={styles.centered}>Kul att se dig, Emma!</StyledText>
+        <StyledText textAlign={'center'} textStyle={'bodySmall'} style={styles.centered}>Hur mår du idag?</StyledText>
+
+        <View style={[styles.centered, {marginTop:100}]}>
+          <ProfilePic profile={require('../assets/profile.png')} emoji={chosenEmoji} size={150}></ProfilePic>
+        </View>
+        <View style={[styles.centered, {marginTop:10}]}>
+          <MoodPicker chosenEmoji={chosenEmoji} setChosenEmoji={setChosenEmoji}/>
+        </View>
+        
+        <View style={styles.bottom}>
+          <View style={{marginBottom:20}}>
+            <Input textInputValue={textInputValue} setTextInputValue={setTextInputValue}/>
+          </View>
+          <Button color={'yellow'} children={'Nästa'} onPress={onPressNext}></Button>
+          <Button color={'transparent'}children={'Inte nu >'} onPress={onPressNotNow}></Button>
+        </View>
+      </View>
     </View>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  centered: {
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    zIndex:1,
+  },
+  bottom: {
+    marginTop: 'auto'
+  },
+  fullHeight: {
+    height:'100%',
+  }
+})
